b4e1e8951765acd948f44af264a1d44768243d46
Piotr Bartnik <32242345+piotrek8080@users.noreply.github.com>
2018-04-29 10:50:32 +0200

    enter on enter

--- hangman/js/onePlayer.js
+++ hangman/js/onePlayer.js
@@ -58,3 +58,16 @@ function othername() {
     window.location.reload(true);
   }
 }
+
+var input = document.getElementById("enter");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
