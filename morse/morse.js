8079b5322f3599ac0df25745a0169f1d3c01beb4
piotrekbartnik <piotr.bartnik@hotmail.com>
2018-10-07 01:45:45 +0200

    second commit

--- morse/morse.js
+++ morse/morse.js
@@ -2,6 +2,8 @@ const textInput = document.getElementById("input");
 const btn = document.getElementById("btn");
 const textOutput = document.getElementById("output");
 
+const morseAlphabet = { "0": "-----", "1": ".----", "2": "..---", "3": "...--", "4": "....-", "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.", "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--..", ".": ".-.-.-", ",": "--..--", "?": "..--..", "!": "-.-.--", "-": "-....-", "/": "-..-.", "@": ".--.-.", "(": "-.--.", ")": "-.--.-" };
+
 function type(text, time) {
   for (let i = 0; i < text.length; i++) {
     let j = i;
@@ -13,23 +15,27 @@ function type(text, time) {
       })(i),
       time * i
     );
-  } 
+  }
 }
-
-
 
 type("Type text to translate here", 100);
 
-
-textInput.addEventListener('keyup', function() {
+textInput.addEventListener("keyup", function() {
   textInput.style.height = "5px";
-  textInput.style.height = (textInput.scrollHeight)+"px";
-  console.log('x')
-})
+  textInput.style.height = textInput.scrollHeight + "px";
+  console.log("x");
+});
 
-function auto_grow(element) {
-  element.style.height = "5px";
-  element.style.height = (element.scrollHeight)+"px";
-  console.log('x')
+function output() {
+  let inputArray = textInput.value.toLowerCase().split("");
+  inputArray.map(a => a === ' ' ? a === 'stop' : false);
+  console.log(inputArray)
+  let outputArray = [];
+  for (let i=0; i <inputArray.length; i++) {
+    console.log(outputArray.push(morseAlphabet[inputArray[i]]))
+
+  }
+  textOutput.innerHTML = outputArray.join('x')
 }
 
+btn.addEventListener('click', output);
