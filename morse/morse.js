fc8c8f97df18963a8e93c62c3c689964328539ae
piotrekbartnik <piotr.bartnik@hotmail.com>
2018-10-07 23:44:11 +0200

    changes

--- morse/morse.js
+++ morse/morse.js
@@ -2,14 +2,14 @@ const textInput = document.getElementById("input");
 const btn = document.getElementById("btn");
 const textOutput = document.getElementById("output");
 
-const morseAlphabet = { "0": "-----", "1": ".----", "2": "..---", "3": "...--", "4": "....-", "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.", "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--..", ".": ".-.-.-", ",": "--..--", "?": "..--..", "!": "-.-.--", "-": "-....-", "/": "-..-.", "@": ".--.-.", "(": "-.--.", ")": "-.--.-" };
+const morseAlphabet = { " ": "/", "0": "-----", "1": ".----", "2": "..---", "3": "...--", "4": "....-", "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.", "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--..", ".": ".-.-.-", ",": "--..--", "?": "..--..", "!": "-.-.--", "-": "-....-", "/": "-..-.", "@": ".--.-.", "(": "-.--.", ")": "-.--.-" };
 
 function type(text, time) {
   for (let i = 0; i < text.length; i++) {
     let j = i;
     setTimeout(
-      (function(x) {
-        return function() {
+      (function (x) {
+        return function () {
           textInput.value += text.charAt(i);
         };
       })(i),
@@ -20,22 +20,35 @@ function type(text, time) {
 
 type("Type text to translate here", 100);
 
-textInput.addEventListener("keyup", function() {
+textInput.addEventListener("click", function () {
+  textInput.value = ""
+});
+
+textInput.addEventListener("keyup", function () {
   textInput.style.height = "5px";
   textInput.style.height = textInput.scrollHeight + "px";
-  console.log("x");
 });
 
 function output() {
   let inputArray = textInput.value.toLowerCase().split("");
-  inputArray.map(a => a === ' ' ? a === 'stop' : false);
-  console.log(inputArray)
   let outputArray = [];
-  for (let i=0; i <inputArray.length; i++) {
-    console.log(outputArray.push(morseAlphabet[inputArray[i]]))
-
+  for (let i = 0; i < inputArray.length; i++) {
+    outputArray.push(morseAlphabet[inputArray[i]])
   }
-  textOutput.innerHTML = outputArray.join('x')
+  textOutput.innerHTML = outputArray.join(' ')
 }
+
+textInput.addEventListener("keydown", function(event) {     
+  if (event.keyCode === 13) {
+    event.preventDefault(); 
+  }
+});
+
+document.addEventListener("keyup", function(event) {  
+  event.preventDefault();  
+  if (event.keyCode === 13) {
+  btn.click();
+  }
+});
 
 btn.addEventListener('click', output);
