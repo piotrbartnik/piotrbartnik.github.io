7ce63d9e4b24acea84865b350474dc628452deb3
piotrbartnik <piotr.bartnik@hotmail.com>
2019-01-30 11:54:51 +0100

    working with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
