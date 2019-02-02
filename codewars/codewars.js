02538a6cf1002d110dfbd02aa63ac0bdc1f228db
piotrbartnik <piotr.bartnik@hotmail.com>
2019-02-02 23:32:10 +0100

    working with api

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -6,17 +6,17 @@
 
 // userAction();
 
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//        // Typical action to be performed when the document is ready:
-//         var response = xhttp.responseText;
-//         console.log("ok"+response);
-//     }
-// };
-// xhttp.open("GET", "https://www.codewars.com/api/v1/users/:PeterB8080", true);
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+        var response = xhttp.responseText;
+        console.log("ok"+response);
+    }
+};
+xhttp.open("GET", "https://xkcd.com/info.0.json", true);
 
-// xhttp.send();
+xhttp.send();
 
 // fetch(request, {mode: 'no-cors'})
 // .then(function(response) {
@@ -24,12 +24,12 @@
 // }).catch(function(error) {  
 //   console.log('Request failed', error)  
 // });
-var request = new Request('https://stats.nba.com/stats/boxscore');
+// var request = new Request('http://xkcd.com/info.0.json');
 
-fetch(request, {mode: 'no-cors'})
-.then(function(response) {
-  console.log(response);
-}).catch(function(error) {  
-  console.log('Request failed', error)  
-});
+// fetch(request, {mode: 'no-cors'})
+// .then(function(response) {
+//   console.log(response);
+// }).catch(function(error) {  
+//   console.log('Request failed', error)  
+// });
 
