9524e681f245d5100abc102fd3922121e068350d
piotrbartnik <piotr.bartnik@hotmail.com>
2019-02-03 00:02:13 +0100

    working with api

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -1,35 +1,13 @@
-// const userAction = async () => {
-//   const response = await fetch('https://www.codewars.com/api/v1/users/:PeterB8080', {mode: 'no-cors'});
-//   const myJson = await response.json(); //extract JSON from the http response
-//   console.log(myJson)
-// }
-
-// userAction();
-
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-        var response = xhttp.responseText;
-        console.log("ok"+response);
+        var response = JSON.parse(xhttp.responseText);
+        console.log(response);
     }
 };
-xhttp.open("GET", "https://xkcd.com/info.0.json", true);
+xhttp.open("GET", "https://airapi.airly.eu/v2/installations/nearest?lat=50.062006&lng=19.940984&maxDistanceKM=3&maxResults=1&apikey=DUTx1OkMGO3PZA782KBdX0ky0SJmkUyK", true);
 
 xhttp.send();
 
-// fetch(request, {mode: 'no-cors'})
-// .then(function(response) {
-//   console.log(response); 
-// }).catch(function(error) {  
-//   console.log('Request failed', error)  
-// });
-// var request = new Request('http://xkcd.com/info.0.json');
-
-// fetch(request, {mode: 'no-cors'})
-// .then(function(response) {
-//   console.log(response);
-// }).catch(function(error) {  
-//   console.log('Request failed', error)  
-// });
 
