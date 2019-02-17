e304608c746ca8c66e4f1e46c2ec1748a684cf34
piotrbartnik <piotr.bartnik@hotmail.com>
2019-02-18 00:32:24 +0100

    wirking

--- hackaton/scripts/index.js
+++ hackaton/scripts/index.js
@@ -2,52 +2,60 @@ const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const accountDetails = document.querySelector('.account-details');
+const adminItems = document.querySelectorAll('.admin');
 
 const setupUI = (user) => {
   if (user) {
+    if (user.admin) {
+      adminItems.forEach(item => item.style.display = 'block');
+    }
     // account info
-    const html = `
-      <div>Logged in as ${user.email}</div>
-    `;
-    accountDetails.innerHTML = html;
-    // toggle UI elements
+    db.collection('users').doc(user.uid).get().then(doc => {
+      const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+        <div class="pink-text">${user.admin ? 'Admin' : ''}</div>
+      `;
+      accountDetails.innerHTML = html;
+    });
+    // toggle user UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
     loggedOutLinks.forEach(item => item.style.display = 'none');
   } else {
-    //hide account info
+    // clear account info
     accountDetails.innerHTML = '';
-    // toggle UI elements
+    // toggle user elements
+    adminItems.forEach(item => item.style.display = 'none');
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
   }
-}
+};
 
 // setup guides
-const setupGuides = (data) => {  
+const setupGuides = (data) => {
 
-  // check if user is logged in
   if (data.length) {
     let html = '';
     data.forEach(doc => {
       const guide = doc.data();
       const li = `
         <li>
-          <div class="collapsible-header grey lighten-4">${guide.title}</div>
-          <div class="collapsible-body white">${guide.content}</div>
+          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
         </li>
       `;
-
-      html += li
+      html += li;
     });
-
-    guideList.innerHTML = html;
+    guideList.innerHTML = html
   } else {
-    guideList.innerHTML = '<h5 class="center-align">Log in to see guides</h5>'
+    guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
   }
-}
+  
+
+};
 
 // setup materialize components
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
